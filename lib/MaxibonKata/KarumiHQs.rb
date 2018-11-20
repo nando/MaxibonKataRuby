7a80fd188c00cdf06dcc41a4fd8816c6efd5bea5
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2018-11-20 12:35:24 +0100

    always more than 2 maxibons in the fridge property

--- lib/MaxibonKata/KarumiHQs.rb
+++ lib/MaxibonKata/KarumiHQs.rb
@@ -1,9 +1,38 @@
 module MaxibonKata
   class KarumiHQs
+    MIN_MAXIBONS = 2
+    MAX_MAXIBONS = 10
+
     attr_reader :maxibonsLeft
 
     def initialize
       @maxibonsLeft = 10
     end
+
+    def openFridge( developer )
+      grab_maxibons developer
+      if should_buy_more_maxibons?
+        buy_maxibons
+      end
+    end
+
+    private
+
+    def should_buy_more_maxibons?
+      @maxibonsLeft <= MIN_MAXIBONS
+    end
+
+    def grab_maxibons( developer )
+      @maxibonsLeft = if developer.maxibonsToGrab > @maxibonsLeft
+                        0
+                      else
+                        @maxibonsLeft - developer.maxibonsToGrab
+                      end
+    end
+
+    def buy_maxibons
+      @maxibonsLeft += MAX_MAXIBONS
+    end
+
   end
 end
--- test/MaxibonKata/KarumiHQs_test.rb
+++ test/MaxibonKata/KarumiHQs_test.rb
@@ -5,8 +5,21 @@ describe MaxibonKata::KarumiHQs do
   it "should start the day with 10 maxibons" do
     property_of {
       MaxibonKata::KarumiHQs.new
-    }.check { |hq|
+    }.check { | hq |
       hq.maxibonsLeft.must_equal 10
+    }
+  end
+
+  it "should always has more than two maxibons in the fridge" do
+    property_of {
+      MaxibonKata::Developer.new sized(12){ string(:alpha) },
+                                 integer
+    }.check { | developer |
+      office = MaxibonKata::KarumiHQs.new
+
+      office.openFridge developer
+
+      office.maxibonsLeft.must_be :>, 2
     }
   end
 
