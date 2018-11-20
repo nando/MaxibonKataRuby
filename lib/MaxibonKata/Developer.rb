3f706c25bf768712634fd38b1ce24c7cc55b6a5b
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2018-11-20 11:06:18 +0100

    amount of maxibons set to every developer property

--- lib/MaxibonKata/Developer.rb
+++ lib/MaxibonKata/Developer.rb
@@ -6,5 +6,27 @@ module MaxibonKata
       @name = name
       @maxibonsToGrab = [0, maxibonsToGrab].max
     end
+
+    class << self
+      def pedro
+        new "Pedro", 3
+      end
+
+      def fran
+        new "Fran", 1
+      end
+
+      def davide
+        new "Davide", 0
+      end
+
+      def sergio
+        new "Sergio", 2
+      end
+
+      def jorge
+        new "Jorge", 1
+      end
+    end
   end
 end
--- test/MaxibonKata/Developer_test.rb
+++ test/MaxibonKata/Developer_test.rb
@@ -13,10 +13,26 @@ describe MaxibonKata::Developer do
   it "should assign the name of the developer in construction" do
     property_of {
       string
-    }.check { |name|
-      MaxibonKata::Developer.new(name).
+    }.check { | name |
+      MaxibonKata::Developer.new( name ).
                              name.must_equal name
     }
   end
 
+  it "should assign the number of maxibons specified to every developer" do
+    {
+      pedro: 3,
+      fran: 1,
+      davide: 0,
+      sergio: 2,
+      jorge: 1
+    }.each do | name, maxibonsToGrab |
+      property_of {
+        [ name, maxibonsToGrab ]
+      }.check { | name, maxibonsToGrab |
+        MaxibonKata::Developer.send( name ).
+                               maxibonsToGrab.must_equal maxibonsToGrab
+      }
+    end
+  end
 end
