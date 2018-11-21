0a00011d58629b443835042f9714004615d00266
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2018-11-21 12:26:18 +0100

    fix test calculate_maxibons_left function

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,10 @@ require "minitest/autorun"
 
 Rantly.default_size = 10
 
-def calculate_maxibons_left(number_of_maxibons, developer)
-  number_of_maxibons.tap do | number_of_maxibons |
-    number_of_maxibons = [ number_of_maxibons - developer.maxibonsToGrab, 0 ].max
-    if number_of_maxibons <= MaxibonKata::KarumiHQs::MIN_MAXIBONS
-      number_of_maxibons += MaxibonKata::KarumiHQs::MAX_MAXIBONS
-    end
+def calculate_maxibons_left(maxibons, developer)
+  number_of_maxibons = [ maxibons - developer.maxibonsToGrab, 0 ].max
+  if number_of_maxibons <= MaxibonKata::KarumiHQs::MIN_MAXIBONS
+    number_of_maxibons += MaxibonKata::KarumiHQs::MAX_MAXIBONS
   end
+  number_of_maxibons
 end
