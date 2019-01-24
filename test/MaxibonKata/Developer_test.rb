3cd987b28b82d110c7a1ddec37d300d7b3b0b98c
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-01-24 12:13:58 +0100

    tests implemetation code removed

--- test/MaxibonKata/Developer_test.rb
+++ test/MaxibonKata/Developer_test.rb
@@ -3,36 +3,14 @@ require "test_helper"
 describe MaxibonKata::Developer do
 
   it "should always grab a positive number of maxibons" do
-    property_of {
-      unpredicteble_developer
-    }.check { | developer |
-      developer.maxibonsToGrab.must_be :>=, 0
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should assign the name of the developer in construction" do
-    property_of {
-      string
-    }.check { | name |
-      MaxibonKata::Developer.new( name ).
-                             name.must_equal name
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should assign the number of maxibons specified to every developer" do
-    {
-      pedro: 3,
-      fran: 1,
-      davide: 0,
-      sergio: 2,
-      jorge: 1
-    }.each do | developerName, maxibonsToGrab |
-      property_of {
-        [ developerName, maxibonsToGrab ]
-      }.check { | developer_name, maxibons_to_grab |
-        MaxibonKata::Developer.send( developer_name ).
-                               maxibonsToGrab.must_equal maxibons_to_grab
-      }
-    end
+    skip "Waiting for your code!!"
   end
 end
--- test/MaxibonKata/KarumiHQs_test.rb
+++ test/MaxibonKata/KarumiHQs_test.rb
@@ -3,118 +3,38 @@ require "test_helper"
 describe MaxibonKata::KarumiHQs do
 
   it "should start the day with 10 maxibons" do
-    property_of {
-      MaxibonKata::KarumiHQs.new
-    }.check { | office |
-      office.maxibonsLeft.must_equal 10
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should always has more than two maxibons in the fridge" do
-    property_of {
-      karumi_developer
-    }.check { | developer |
-      office = MaxibonKata::KarumiHQs.new
-
-      office.openFridge developer
-
-      office.maxibonsLeft.must_be :>, 2
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should buy 10 more maxibons if there are less than 3 in the fridge" do
-    property_of {
-      hungry_developer
-    }.check { | developer |
-      office = MaxibonKata::KarumiHQs.new
-      initial_maxibons = office.maxibonsLeft
-
-      office.openFridge developer
-
-      expected_maxibons = calculate_maxibons_left( initial_maxibons,
-                                                   developer )
-
-      office.maxibonsLeft.must_equal expected_maxibons
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should request 10 more maxibons using the chat if there are less than 3 in the fridge" do
-    property_of {
-      hungry_developer
-    }.check { | developer |
-      chat = MaxibonKata::Chat.new
-      office = MaxibonKata::KarumiHQs.new( chat )
-
-      office.openFridge developer
-
-      chat.messageSent.must_equal "Hi guys, I'm #{ developer.name }. We need more maxibons!"
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should never request more maxibons to the team using the chat if there are more than 2 in the fridge" do
-    property_of {
-      not_so_hungry_developer
-    }.check { | developer |
-      chat = MaxibonKata::Chat.new
-      office = MaxibonKata::KarumiHQs.new( chat )
-
-      office.openFridge developer
-
-      chat.messageSent.must_be_nil
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should always has more than two maxibons in the fridge even if some karumies grab maxibons in group" do
-    property_of {
-      karumies_group
-    }.check { | developers |
-      office = MaxibonKata::KarumiHQs.new
-
-      office.openFridge developers
-
-      office.maxibonsLeft.must_be :>, 2
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should buy 10 more maxibons if there are less than 2 in the fridge when grabbing maxibons in group" do
-    property_of {
-      developers_group
-    }.check { | developers |
-      office = MaxibonKata::KarumiHQs.new
-      initial_maxibons = office.maxibonsLeft
-
-      office.openFridge developers
-
-      expected_maxibons = calculate_maxibons_left( initial_maxibons,
-                                                   developers )
-
-      office.maxibonsLeft.must_equal expected_maxibons
-    }
+    skip "Waiting for your code!!"
   end
 
   it "should request 10 more maxibons using the chat if there are less than 3 in the fridge when grabbing maxibons in group" do
-    property_of {
-      hungry_group
-    }.check { | developers |
-      chat = MaxibonKata::Chat.new
-      office = MaxibonKata::KarumiHQs.new( chat )
-
-      office.openFridge developers
-
-      chat.messageSent.must_include "We need more maxibons!"
-    }
+    skip "Waiting for your code!!"
   end
 
   it "never request more maxibons to the team using the chat if there are more than 2 in the fridge when grabbing maxibons in group" do
-    property_of {
-      developers_who_wont_grab_all_maxibons
-    }.check { | developers |
-      chat = MaxibonKata::Chat.new
-      office = MaxibonKata::KarumiHQs.new( chat )
-
-      office.openFridge developers
-
-      chat.messageSent.must_be_nil
-    }
+    skip "Waiting for your code!!"
   end
 end
